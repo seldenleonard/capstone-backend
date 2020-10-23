436c273601e12f3425f9af40922fbaea2fa3d79d
seldenleonard <selden222@gmail.com>
2020-10-23 12:18:13 -0700

    seeds database

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
 
   has_many :artworks
   has_many :upvotes
-  belongs_to :college
+  belongs_to :college, optional: true
+  has_many :upvoted_artworks, through: :upvotes, source: :artwork
 
 end
