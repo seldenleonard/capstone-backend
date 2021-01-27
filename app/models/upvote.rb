5768d1ad9a48bcda1aa5b494847241c836a2f94f
seldenleonard <selden222@gmail.com>
2021-01-27 02:48:34 -0500

    edits commented-out code in upvote.rb

--- app/models/upvote.rb
+++ app/models/upvote.rb
@@ -1,6 +1,6 @@
 class Upvote < ApplicationRecord
 
-# I dont need validations on foreign keys, because they are already required by rails
+# NOTE: No need for validations on foreign keys, because they are already required by Rails
 
   validates :user_id, :uniqueness => { :scope => :artwork_id,
     :message => "You have already upvoted this artwork." }
