e907f05a503713c8f3c14dece78de3833ef45b55
seldenleonard <selden222@gmail.com>
2021-01-27 03:05:34 -0500

    deletes example in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
 
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     
     # User Routes
