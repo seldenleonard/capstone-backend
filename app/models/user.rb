5a32240cc5bd63c9878d68640f7b062a1e411fe3
seldenleonard <selden222@gmail.com>
2021-01-27 02:49:43 -0500

    deletes commented-out code from user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,19 +5,14 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :update }
-  # -- trying to have email include .edu or maybe a list of @usc.edu, @nyu.edu, @utau.edu, and @umich.edu?
   validates :name, presence: true
   validates :name, length: { in: 2..50 }
-  # validates :artist, presence: true # -- Probably dont need because a checkbox will make it unneccesary on frontend
-  validates :artist, inclusion: { in: [true, false] } # this is not stopping random words from being entered into the artist params in User create
-  # validates :artist, exclusion: { in: [nil] }
-  # validates :artist, default: false
+  validates :artist, inclusion: { in: [true, false] }
   validates :bio, length: { maximum: 500 }, if: :is_artist?
   validates :art_style, length: { maximum: 50 }, if: :is_artist?
   validates :major, presence: true, if: :is_artist?
   validates :major, length: { maximum: 30 }, if: :is_artist?
   validates :minor, length: { maximum: 30 }, if: :is_artist?
-  # validates :graduation_year, presence: true ------ need to make this for artists only -- can do this on frontend
   validates :graduation_year, length: { is: 4 }, if: :is_artist?
   validates :college_id, presence: true, if: :is_artist?
 
